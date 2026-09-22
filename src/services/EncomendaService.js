c2059247c723e4fc9dd4cd4ccdfa37e3357ff110
Call333 <cam4@aluno.ifal.edu.br>
2026-09-22 14:52:53 -0300

    feat(EncomendaService): Implementando a funcao criarEncomenda()

---
+++ src/services/EncomendaService.js
@@ -0,0 +1,17 @@
+import { ValidationError } from "../utils/errors/ValidationError";
+
+export class EncomendaService {
+    constructor(encomendaRepository){
+        this.encomendaRepository = encomendaRepository;
+    }
+
+    async criarEncomenda(dadosEncomenda){
+        const encomendaValidacao = new Encomenda(dadosEncomenda);
+
+        if(!encomendaValidacao.isValid()) {
+            throw new ValidationError("Os Campos descricao, origem e destino são obrigatórios.")
+        }
+
+        return await this.encomendaRepository.create(encomendaValidacao);
+    }
+}
