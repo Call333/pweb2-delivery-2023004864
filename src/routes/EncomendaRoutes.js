067654c4ac189e8a278d556f765abf3b3b55fc40
Call333 <cam4@aluno.ifal.edu.br>
2026-09-24 13:02:04 -0300

    feat(EncomendaRoutes.js): Adicionando o endpoint '/entregas/:id/cancelar' para cancelar encomendas.

--- src/routes/EncomendaRoutes.js
+++ src/routes/EncomendaRoutes.js
@@ -13,7 +13,11 @@ const encomendaService = new EncomendaService(encomendaRepository);
 const encomendaController = new EncomendaController(encomendaService);
 
 router.post('/entregas', encomendaController.criar);
+
 router.get('/entregas', encomendaController.encontrarTodos);
+
 router.patch('/entregas/:id/avancar', encomendaController.avancar);
+router.patch('/entregas/:id/cancelar', encomendaController.avancar);
+
 
 export default router;
