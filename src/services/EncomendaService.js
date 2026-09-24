0d227b778ccae646bdd1886795db36f760f3c891
Call333 <cam4@aluno.ifal.edu.br>
2026-09-24 07:34:19 -0300

    feat(EncomendaService.js): Agora não é mais possível enviar encomendas com origem e destino iguais.

--- src/services/EncomendaService.js
+++ src/services/EncomendaService.js
@@ -8,9 +8,11 @@ export class EncomendaService {
 
     async criarEncomenda(dadosEncomenda){
         const encomendaValidacao = new Encomenda(dadosEncomenda);
-
+        
         if(!encomendaValidacao.isValid()) {
             throw new ValidationError("Os Campos descricao, origem e destino são obrigatórios.")
+        } else if(encomendaValidacao.origem === encomendaValidacao.destino) {
+            throw new ValidationError("O destino e a origem não podem ser iguais.")
         }
 
         return await this.encomendaRepository.create(encomendaValidacao);
